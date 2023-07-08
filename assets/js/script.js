a4a5215f4615b2019a46d0414e31faaac2a11e2e
barre <barrenac@oregonstate.edu>
2023-07-07 19:29:31 -0700

    Arranged files to appropiate folders, commented script file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var searchButton = document.querySelector('#search-btn');
 var cityInput = document.querySelector('#city-search');
 
+// API runs lat and lon inputs and outputs weather data
 function getCoordApi(lat, lon) {
-    
     var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=24551ac99b969231c8ae35b5ac552a6c&units=imperial";
     fetch(weatherURL)
         .then(function(response) {
@@ -13,27 +13,37 @@ function getCoordApi(lat, lon) {
         });
 }
 
-getCoordApi(51.5073219, -0.1276474);
-
-// This works, now I need to find a way to get the lat and lon from array to plug into getCoordApi
+// This API function extracts lat and lon data from API to plug into getCoordApi function
 function getCityApi(cityName){
-    var citySearchURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid=24551ac99b969231c8ae35b5ac552a6c";
+    var citySearchURL = "https://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid=24551ac99b969231c8ae35b5ac552a6c";
     fetch(citySearchURL)
         .then(function(response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            // calls other API function and plugs in lat and lon variables
+            getCoordApi(lat, lon);
         })
 }
 
-// function prevents default and takes input to plug into getCityApi function
+// function prevents default and takes text input to plug into getCityApi function
 var formSubmitHandler = function (event){
     event.preventDefault();
     var citySearched = cityInput.value.trim();
+    // conditional checks for valid input and clears out input box
     if (citySearched){
         getCityApi(citySearched);
         cityInput.value = '';
+        // checks local storage for previous searches, if none, new array made.
+        let cityarr = JSON.parse(localStorage.getItem("city")) || [];
+        console.log(cityarr);
+        cityarr.push(citySearched)
+
+        localStorage.setItem("city", JSON.stringify(cityarr));
+        console.log(cityarr)
     // returns alert if nothing is searched
     } else {
         alert('Please enter a city name.')
